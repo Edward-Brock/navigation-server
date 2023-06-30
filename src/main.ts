3dd9ae68b64870a41c96524a8c61c30641bb6feb
DESKTOP-4DBQPSO\14164 <1416477069@qq.com>
2023-06-30 17:44:26 +0800

    变更 - 更新 version 版本 - 优化 项目启动时展示当前各端口

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,13 @@ async function bootstrap() {
   app.use(express.urlencoded({ extended: true }));
   app.enableCors();
   const configService: ConfigService = app.get<ConfigService>(ConfigService);
-  const port = configService.get("SERVER_PORT");
-  await app.listen(port);
+  const http_port = configService.get("SERVER_PORT");
+  const wss_port = configService.get("WEBSOCKET_SERVER_PORT");
+  await app.listen(http_port);
   console.log(`
   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  Navigation Server 服务已启动：http://localhost:${port}
+  Navigation Server HTTP 服务已启动：http://localhost:${http_port}
+  Navigation Server WebSocket 服务已启动：http://localhost:${wss_port}
   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
   `);
 }
